656df39b4d9efd6f21b1c49581159f1be091df2b
J22T <jordan22taylor@gmail.com>
2022-10-23 20:56:09 -0700

    Small error

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -32,7 +32,7 @@ const Cards = () => {
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>Group Account</h2>
-              <p className='text-center text-4xl font-bold'>$1299</p>
+              <p className='text-center text-4xl font-bold'>$299</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>5 TB Storage</p>
                   <p className='py-2 border-b mx-8'>10 Granted Users</p>
